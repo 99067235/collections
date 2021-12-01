0a70aeb47c144b7efc6e6f3f976a42f1bb23dd11
unknown <99067235@mydavinci.nl>
2021-12-01 12:14:00 +0100

    rekenen met lists afgemaakt

--- Rekenen met lists.py
+++ Rekenen met lists.py
@@ -1,2 +1,55 @@
 listOne = ['1','2','3','4','5','6','7','8','9','10']
 listTwo = ['2','4','6','8','10','12','14','16','18','20']
+
+# 2 getallen optellen
+def addAndDisplayLists(getal):
+    getal1 = int(listOne[getal])
+    getal2 = int(listTwo[getal])
+    print(getal1, "+", getal2, "=", getal1 + getal2)
+
+print("")
+print("")
+print("---------")
+print("Add lists")
+for i in range(10):
+    addAndDisplayLists(i)
+
+# 2 getallen van elkaar aftrekken
+def substractAndDisplayLists(getal):
+    getal1 = int(listOne[getal])
+    getal2 = int(listTwo[getal])
+    print(getal1, "-", getal2, "=", getal1 - getal2)
+
+print("")
+print("")
+print("---------------")
+print("Substract lists")
+for i in range(10):
+    substractAndDisplayLists(i)
+
+# 2 getallen keer elkaar
+
+def multiplyAndDisplayLists(getal):
+    getal1 = int(listOne[getal])
+    getal2 = int(listTwo[getal])
+    print(getal1, "X", getal2, "=", getal1 * getal2)
+
+print("")
+print("")
+print("---------------")
+print("Multiply lists")
+for i in range(10):
+    multiplyAndDisplayLists(i)
+
+# 2 getallen delen
+def divideAndDisplayLists(getal):
+    getal1 = int(listOne[getal])
+    getal2 = int(listTwo[getal])
+    print(getal1, ":", getal2, "=", getal1 / getal2)
+
+print("")
+print("")
+print("---------------")
+print("divide lists")
+for i in range(10):
+    divideAndDisplayLists(i)
