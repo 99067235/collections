a0ce4e34ff32116c4fd19574f009a3b953f0b6b8
unknown <99067235@mydavinci.nl>
2021-12-07 14:39:04 +0100

    bezig met MenM

--- MM.py
+++
@@ -1,1 +0,0 @@
-hoeveel = int(input("Hoeveel"))
--- MenM.py
+++ MenM.py
@@ -1,24 +1,43 @@
 import random
 zak = []
+zak2 = {}
 hoeveel = 0
 
 colors = ["oranje", "blauw", "groen", "bruin"]
 
+
+def choice():
+    global i
+    for i in range(hoeveel):
+        new = random.choice(colors)
+        if new in zak2:
+            zak2[new] += 1
+        else:
+            zak2[new] = 1
+    return zak2
+
+        
 def snapNiet():
     print("Sorry dat snap ik niet...")
     print("")
 
 def hoeveelstuks():
     global hoeveel
-    hoeveel = int(input("Hoeveel M&M's wil je in je zakje? "))
-    if hoeveel >= 0:
-        toevoegen()
-    else:
+    try:
+        hoeveel = int(input("Hoeveel M&M's wil je in de zak van Sinterklaas? "))
+        if hoeveel >= 0:
+            print(toevoegen(hoeveel))
+            choice()
+            int(input("Hoeveel M&M's wil je in je zakje? "))
+        else:
+            snapNiet()
+            hoeveelstuks()
+    except ValueError:
         snapNiet()
         hoeveelstuks()
 
-def toevoegen():
-    for i in range(hoeveel):
+def toevoegen(aantal):
+    for i in range(aantal):
         new = random.choice(colors)
         zak.append(new)
     return zak
